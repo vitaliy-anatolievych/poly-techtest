a333acf4dda50deeaf314a52391e900ea845b39a
vitaliy-anatolievych <vitaliy.anatolyevich@gmail.com>
2023-10-05 03:35:21 +0300

    test

--- app/src/main/java/com/testtask/polytech/app/App.kt
+++ app/src/main/java/com/testtask/polytech/app/App.kt
@@ -11,7 +11,6 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
         startKoin {
             androidContext(this@App)
             modules(listOf(presentationModule, domainModule, dataModule))
